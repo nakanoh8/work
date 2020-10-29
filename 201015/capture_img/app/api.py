c6aa08b26080abe6368e7cc2d46c9e6e519f0695
nakanoh <murry.xxx.7@gmail.com>
2020-10-29 23:39:45 +0900

    Resolve CORS Error

--- 201015/capture_img/app/api.py
+++ 201015/capture_img/app/api.py
@@ -1,15 +1,24 @@
 # -*- coding: utf-8 -*-
 from flask import Flask, request, make_response, render_template, url_for
-
-from . import service
+from flask_cors import CORS, cross_origin
+# from . import service
 
 api = Flask(__name__)
+CORS(api, support_credentials=True)
 
 @api.route('/', methods=['GET'])
 def index():
-    return render_template('index.html')
+    # return render_template('index.html')
+    return "GET"
 
-@api.route('/capture_img', methods=['POST'])
-def capture_img():
-    msg = service.save_img(request.form["img"])
-    return make_response(msg)
+# @api.route('/capture_img', methods=['POST'])
+# def capture_img():
+#     msg = service.save_img(request.form["img"])
+#     return make_response(msg)
+
+@api.route('/test', methods=['POST'])
+def test():
+    print("############")
+    print(request.form["test"])
+    res = request.form["test"]
+    return make_response({"test": res + " :::OK"})
