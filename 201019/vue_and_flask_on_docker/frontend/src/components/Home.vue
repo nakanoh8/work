ca7620163920955cec8f8608ca900f26980e73cc
nakanoh <murry.xxx.7@gmail.com>
2020-10-30 18:35:54 +0900

    DockerComposeハンズオン完了

--- 201019/vue_and_flask_on_docker/frontend/src/components/Home.vue
+++ 201019/vue_and_flask_on_docker/frontend/src/components/Home.vue
@@ -4,10 +4,10 @@
     <p>Home</p>
     <button @click="getRandom">占う</button>
     <p>Random number from backend: {{ randomNum }}</p>
-    <h1 v-if="randomNumber%4==0">Awesome!!!</h1>
-    <h2 v-if="randomNumber%4==1">Good</h2>
-    <h2 v-if="randomNumber%4==2">Bad...</h2>
-    <h1 v-if="randomNumber%4==3">S〇〇ks!!!</h1>
+    <h1 v-if="randomNum%4==0">Awesome!!!</h1>
+    <h2 v-if="randomNum%4==1">Good</h2>
+    <h2 v-if="randomNum%4==2">Bad...</h2>
+    <h1 v-if="randomNum%4==3">S〇〇ks!!!</h1>
   </div>
 </template>
 
