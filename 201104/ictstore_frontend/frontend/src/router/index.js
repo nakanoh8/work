2bb7f6b9f0abfb3654cb102dcb8603a856e9d586
nakanoh <murry.xxx.7@gmail.com>
2020-11-09 00:28:40 +0900

    Update pages, route/index.js

--- 201104/ictstore_frontend/frontend/src/router/index.js
+++ 201104/ictstore_frontend/frontend/src/router/index.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 import auth from '@/pages/auth'
+import AdminLogin from '@/pages/AdminLogin'
+import AdminMenu from '@/pages/AdminMenu'
+import UserInfo from '@/pages/UserInfo'
+import FaceRegist from '@/pages/FaceRegist'
+import FacePreview from '@/pages/FacePreview'
 
 Vue.use(Router)
 
@@ -11,13 +15,33 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    },
-    {
-      path: '/auth',
       name: 'auth',
       component: auth
+    },
+    {
+      path: '/AdminLogin',
+      name: 'AdminLogin',
+      component: AdminLogin
+    },
+    {
+      path: '/AdminMenu',
+      name: 'AdminMenu',
+      component: AdminMenu
+    },
+    {
+      path: '/UserInfo',
+      name: 'UserInfo',
+      component: UserInfo
+    },
+    {
+      path: '/FaceRegist',
+      name: 'FaceRegist',
+      component: FaceRegist
+    },
+    {
+      path: '/FacePreview',
+      name: 'FacePreview',
+      component: FacePreview
     }
   ]
 })
